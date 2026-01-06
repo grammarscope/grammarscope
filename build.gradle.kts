0457e3d45e921395ea2588a498a5dca92e8b790c
1313ou <1313ou@gmail.com>
2026-01-06 09:37:25 +0100

    bump to 98

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ buildscript {
 allprojects {
 
     ext {
-        set("versionCode", 6097)
-        set("versionName", "6.97")
+        set("versionCode", 6098)
+        set("versionName", "6.98")
         set("minSdk", 24)
         set("targetSdk", 36)
         set("compileSdk", 36)
