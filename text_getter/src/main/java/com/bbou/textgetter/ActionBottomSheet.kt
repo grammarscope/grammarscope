a9146534e793a6a4171460aac409429d1ea22027
1313ou <1313ou@gmail.com>
2026-03-27 09:24:50 +0100

    color of nav bar

--- text_getter/src/main/java/com/bbou/textgetter/ActionBottomSheet.kt
+++ text_getter/src/main/java/com/bbou/textgetter/ActionBottomSheet.kt
@@ -4,19 +4,33 @@
 
 package com.bbou.textgetter
 
+import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.WindowInsetsControllerCompat
 import com.bbou.textrecog.R
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.color.MaterialColors.getColor
 import org.depparse.NightMode.isNightMode
+import com.google.android.material.R as MaterialR
 
 class ActionBottomSheet(val sentence: String, val select: (sentence: String) -> Unit) : BottomSheetDialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener {
+            val bottomSheet = dialog.findViewById<View>( MaterialR.id.design_bottom_sheet)
+            bottomSheet?.background = getColor(requireContext(), MaterialR.attr.colorSecondary, Color.TRANSPARENT).toDrawable()
+        }
+        return dialog
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +42,9 @@ class ActionBottomSheet(val sentence: String, val select: (sentence: String) -> 
             dismiss()
             select.invoke(sentence)
         }
-            //view.findViewById<Button>(R.id.btn_cancel).setOnClickListener {
-            //    dismiss()
-            //}
+        //view.findViewById<Button>(R.id.btn_cancel).setOnClickListener {
+        //    dismiss()
+        //}
         return view
     }
 
